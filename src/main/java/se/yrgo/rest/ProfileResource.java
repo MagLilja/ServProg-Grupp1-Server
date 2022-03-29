41a29687b7534a6628b6fd3b335cf6074b37b46b
Magnus Lilja <magnus@connoisseur.se>
2022-03-29 19:17:34 +0200

    Added javadoc to the ProfileResource.java

--- src/main/java/se/yrgo/rest/ProfileResource.java
+++ src/main/java/se/yrgo/rest/ProfileResource.java
@@ -5,7 +5,6 @@ import se.yrgo.service.ProfileManagementService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.resource.spi.IllegalStateException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -15,30 +14,45 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 import java.util.List;
 
 /**
+ * REST resource class for a social media profile
+ *
  * @Author - Andreas Karlsson
+ * @Author - Magnus Lilja
  */
-
 @Stateless
 @Path("/profiles")
 @Produces("application/JSON")
 @Consumes("application/JSON")
 public class ProfileResource {
 
+    /**
+     *
+     */
     @Inject
     private ProfileManagementService service;
 
+    /**
+     * The base GET resource.
+     * @return a list of all profiles in the database
+     */
     @GET
     public List<Profile> getAllProfiles() {
         return service.getAllProfiles();
     }
 
+    /**
+     * The search GET resource.
+     * /search?firstname=firstName
+     * or /search?lastname=lastName
+     * or /search?lastname=lastName&firstname=firstName
+     * @param firstName - Query parameter from the URI
+     * @param lastName
+     * @return a list of profiles with first- and/or lastnames containing the search params
+     */
     @GET
     @Path("/search")
     public List<Profile> getProfilesByQuery(@QueryParam("firstname") String firstName,
@@ -46,54 +60,54 @@ public class ProfileResource {
         return service.getProfilesByQuery(firstName, lastName);
     }
 
-//    @GET
-//    @Path("/query")
-//    public List<Profile> getProfilesByQuery(@QueryParam("firstname") String firstName,
-//                                            @QueryParam("lastname") String lastName) {
-//        return service.getProfilesByFirstNameAndLastName();
-//    }
-
-//    SELECT profile FROM Profiles as profile WHERE firstName = :firstname
-
-//    http://164.92.231.95:8080/Grupp1ServerApplication/api/profiles/query?firstname=andreas&lastname=karlsson
-//    http://164.92.231.95:8080/Grupp1ServerApplication/api/profiles/query?firstname=andreas
-//    http://164.92.231.95:8080/Grupp1ServerApplication/api/profiles/query?lastname=karlsson
-
-//- Alla profiler på förnamn  /profiles/query?firstname=andreas
-//- Alla profiler på efternamn
-//- Alla profiler på fulla namn
-
+    /**
+     * GET resource for getting a profile by ID
+     * @param id - the id of the profile
+     * @return a profile matching the id.
+     */
     @GET
     @Path("{id}")
     public Profile findProfileById(@PathParam("id") int id) {
         return service.getById(id);
     }
 
-
+    /**
+     * GET resource for getting a profile by the unique username
+     * @param userName - the username of the profile
+     * @return a profile matching the username.
+     */
     @GET
     @Path("{username}")
-    public Profile findProfileByUsername(@PathParam("username") int id) {
+    public Profile findProfileByUsername(@PathParam("username") int userName) {
         // TODO
         return null;
     }
 
+    /**
+     * POST resource for creating a new profile in the database
+     * Accepts a JSON string matching the Profile class
+     * If lastname, firstname or username is missing it returns a BAD REQUEST
+     * @param newProfile - the profile to be created
+     * @return
+     */
     @POST
-    @Produces("application/JSON")
-    @Consumes("application/JSON")
-    public Response createProfile(Profile profile) {
-        if (profile.getLastName() == null || profile.getFirstName() == null || profile.getUserName() == null) {
+    public Response createProfile(Profile newProfile) {
+        if (newProfile.getLastName() == null || newProfile.getFirstName() == null || newProfile.getUserName() == null) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Status: 400 Bad Request - Required Fields Missing").build();
         }
-        service.registerProfile(profile);
-        return Response.status(Response.Status.ACCEPTED).entity(profile).build();
+        service.registerProfile(newProfile);
+        return Response.status(Response.Status.ACCEPTED).entity(newProfile).build();
     }
 
+    /**
+     * DELETE resource to remove a profile from the database.
+     * @param id -
+     */
     @DELETE
     @Path("{id}")
     public void deleteProfileById(@PathParam("id") int id) {
         service.deleteProfileById(id);
     }
-
 
     @PUT
     @Path("{id}")
